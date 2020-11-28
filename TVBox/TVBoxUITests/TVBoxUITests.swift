2d92e7dc33e99cd57f9eb096e8f476d8f99f6f86
Mohamed Sheta <mohamed.sheta@syngenta.com>
2020-11-28 13:28:59 +0200

    UItests

--- TVBox/TVBoxUITests/TVBoxUITests.swift
+++ TVBox/TVBoxUITests/TVBoxUITests.swift
@@ -8,35 +8,35 @@
 import XCTest
 
 class TVBoxUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    func testListViewsSectionsNumbers() {
         let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let tablesQuery = XCUIApplication().tables
+        sleep(3)
+        XCTAssertTrue(tablesQuery.cells.count > 0)
+    }
+    
+    func testRateButton_IsExist() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        sleep(3)
+        app.tables/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"The Mandalorian").element/*[[".cells.containing(.staticText, identifier:\"2019-11-12\").element",".cells.containing(.staticText, identifier:\"8.5\").element",".cells.containing(.staticText, identifier:\"The Mandalorian\").element"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let rateButton = app.buttons["RateButton"]
+        XCTAssertTrue(rateButton.exists)
+    }
+    
+    func testRatePressed_ButtonDisabled() {
+        let app = XCUIApplication()
+        app.launch()
+        sleep(2)
+        app.tables/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"The Mandalorian").element/*[[".cells.containing(.staticText, identifier:\"2019-11-12\").element",".cells.containing(.staticText, identifier:\"8.5\").element",".cells.containing(.staticText, identifier:\"The Mandalorian\").element"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let rateButton = app.buttons["RateButton"]
+        rateButton.tap()
+        sleep(2)
+        XCTAssertTrue(rateButton.isEnabled == false)
+        
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
